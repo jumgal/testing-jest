61ada32fccade15a28939307effa2d81813b09ee
Jumgalbek Arymbaev <Jumgalbek.Arymbaev@ulta.com>
2021-11-14 16:42:44 -0600

    test code before redux

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,7 +9,38 @@ beforeEach(() => {
     wrapped = mount(<CommentBox />)
 })
 
+
+afterEach(() => {
+    wrapped.unmount()
+})
+
 it('it has a textare and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
+})
+
+
+describe('the text area', () => {
+
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        })
+
+        wrapped.update()
+    })
+
+    it('has a text area that users can type in', () => {
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    })
+
+    it('when form is submitted, text area gets emptied', () => {
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+
+        wrapped.find('form').simulate('submit')
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
 })
